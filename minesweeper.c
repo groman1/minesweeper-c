2fad5511bdbffba23aa4d301135be70cdf4e5ce1
Sergey <sergeiroman060@gmail.com>
2024-12-24 22:05:17 +0200

    1.11: Added default presets, fixed winning with spamming on single tile, added retry with the same settings

--- minesweeper.c
+++ minesweeper.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct field
 {
@@ -13,7 +14,6 @@ struct field field;
 
 int genrandom();
 int genfield();
-int clrgetch();
 int revealmines();
 int checkmines(int location);
 int draw_frame(int maxx, int maxy);
@@ -22,76 +22,110 @@ int contains(int arr[], int chkval, int len);
 
 int main()
 {
-    printf("Enter the width of the playfield: ");
-    scanf("%d", &fieldwidth);
-    printf("Enter the height of the playfield: ");
-    scanf("%d", &fieldheight);
-    printf("Enter the quantity of mines: ");
-    scanf("%d", &minescount);
+    printf("Select from the available presets:\n\
+    small: 8 by 8, 10 mines\n\
+    medium: 16 by 16, 40 mines\n\
+    large: 24 by 24, 90 mines\n\
+    manual: or choose your own settings: ");
+    char presets[8];
+    scanf("%s", &presets);
+
+    if(!strcmp(presets,"small")){
+        fieldwidth = 8;
+        fieldheight = 8;
+        minescount = 10;
+    }
+    else if(!strcmp(presets,"medium"))
+    {
+        fieldwidth = 16;
+        fieldheight = 16;
+        minescount = 40;
+    }
+    else if(!strcmp(presets,"large"))
+    {
+        fieldwidth = 24;
+        fieldheight = 24;
+        minescount = 90;
+    }
+    else 
+    {
+        printf("Enter the width of the playfield: ");
+        scanf("%d", &fieldwidth);
+        printf("Enter the height of the playfield: ");
+        scanf("%d", &fieldheight);
+        printf("Enter the quantity of mines: ");
+        scanf("%d", &minescount);
+    }
+
+    
 
     if(minescount>fieldheight*fieldwidth){printf("Too many mines, exiting..."); return 1;}
 
     initscr();
-    curs_set(2);
     noecho();
     keypad(stdscr,1);
-
-    int curslocation=0;
-    int mines = 0;
-    int selectedsquares = 0;
-
-    genfield();
-    printw("\n");
-    /*for(int i = 0; i<minescount;++i)
+    int stop = 0;
+    while (!stop)
     {
-        printw("%d\n", field.mines[i]);          //     For testing the mines locations
-    }*/
-    int end = 0;
-    int maxx,maxy;
-    getmaxyx(stdscr, maxy, maxx);
-    if(maxx<fieldwidth||maxy<fieldheight){printf("The field is too big, exiting...");free(field.mines);endwin();return 1;}
-    draw_frame(maxx, maxy);
+        clear();
+        curs_set(2);
+        int curslocation = 0;
+        int selectedsquares = 0;
 
-    while(!end)
-    {
-        while(!end){
-            move(curslocation/fieldwidth+maxy/2-fieldheight/2,curslocation%fieldwidth+maxx/2-fieldwidth/2);
-            int ch = getch();
-            if(curslocation/fieldwidth>0&&ch==259){curslocation-=fieldwidth;} // UP
-            else if(curslocation/fieldwidth<fieldheight-1&&ch==258){curslocation+=fieldwidth;} // DOWN
-            else if(curslocation%fieldwidth>0&&ch==260){curslocation-=1;} // LEFT
-            else if(curslocation%fieldwidth<fieldwidth-1&&ch==261){curslocation+=1;} // RIGHT
-            else if(ch==330){endwin();free(field.mines); return 0;}
-            else if(ch==32){break;}
-            else if(ch==331){printw("M");}
-            //mvprintw(0,0,"      ");
-            //mvprintw(0,0,"%d",curslocation);  // DEBUG: for getting curret cursor location
-        }
-        selectedsquares++;
-        if((checkmines(curslocation)!=-1))
+        genfield();
+
+        /*printw("\n");
+        for(int i = 0; i<minescount;++i)
         {
-            printw("%d", checkmines(curslocation));
-        }
-        else
+            printw("%d\n", field.mines[i]);          //     For testing the mines locations
+        }*/
+
+        int cont = 1;
+        int maxx,maxy;
+        getmaxyx(stdscr, maxy, maxx);
+        if(maxx<fieldwidth||maxy<fieldheight){printf("The field is too big, exiting...");free(field.mines);endwin();return 1;}
+        draw_frame(maxx, maxy);
+
+        while(cont)
         {
-            revealmines();
-            mvprintw(1,maxx/2-4,"You lost");
-            curs_set(0);
-            getch();    
-            end=1;        
+            while(cont){
+                move(curslocation/fieldwidth+maxy/2-fieldheight/2,curslocation%fieldwidth+maxx/2-fieldwidth/2);
+                int ch = getch();
+                if(curslocation/fieldwidth>0&&ch==259){curslocation-=fieldwidth;} // UP
+                else if(curslocation/fieldwidth<fieldheight-1&&ch==258){curslocation+=fieldwidth;} // DOWN
+                else if(curslocation%fieldwidth>0&&ch==260){curslocation-=1;} // LEFT
+                else if(curslocation%fieldwidth<fieldwidth-1&&ch==261){curslocation+=1;} // RIGHT
+                else if(ch==330){endwin();free(field.mines); return 0;}
+                else if(ch==32){break;}
+                else if(ch==331){printw("M");}
+                //mvprintw(0,0,"      ");
+                //mvprintw(0,0,"%d",curslocation);  // DEBUG: for getting curret cursor location 
+            }
+            if(inch()==' ') {selectedsquares++;}
+            if((checkmines(curslocation)!=-1))
+            {
+                printw("%d", checkmines(curslocation));
+            }
+            else
+            {
+                revealmines();
+                mvprintw(1,maxx/2-4,"You lost");
+                curs_set(0);
+                cont = 0;
+                if(getch()!=114) {stop = 1;}        
+            }
+            if(selectedsquares==fieldwidth*fieldheight-minescount)
+            {
+                revealmines();
+                mvprintw(1,maxx/2-8,"Congratulations!");
+                curs_set(0);
+                cont = 0;
+                if(getch()!=114) {stop = 1;}  
+            }
+
         }
-        if(selectedsquares==fieldwidth*fieldheight-minescount)
-        {
-            revealmines();
-            mvprintw(1,maxx/2-8,"Congratulations!");
-            curs_set(0);
-            getch();
-            end=1;
-        }
-        
+        free(field.mines);
     }
-    //printf("%d", curslocation);
-    free(field.mines);
     endwin();
     return 0;
 }
@@ -195,19 +229,15 @@ int checkmines(int location)
     return returnmines;
 }
 
-int clrgetch()
-{
-    while(!getch());
-}
-
 int draw_frame(int maxx, int maxy)
 {
-    //mvprintw(maxy/2-6,maxx/2-7, "==============");
     for(int i = 0; i<fieldwidth; ++i,mvprintw(maxy/2-fieldheight/2-1,maxx/2-fieldwidth/2-1+i, "="));
+
     for(int i = 0;i<fieldheight+2;++i){
         mvprintw(maxy/2-fieldheight/2+i-1,maxx/2-fieldwidth/2-2, "||");
         mvprintw(maxy/2-fieldheight/2+i-1,maxx/2+fieldwidth/2+fieldwidth%2, "||");
     }
+
     for(int i = 0; i<fieldwidth; ++i,mvprintw(maxy/2+fieldheight/2+fieldheight%2,maxx/2-fieldwidth/2-1+i, "="));
 }
 
@@ -217,7 +247,6 @@ int revealmines()
     getmaxyx(stdscr,maxy, maxx);
 
     for(int i = 0; i<minescount;++i){
-                //wind height / 2 - field height / 2 + mineheight     wind width / 2 - field width / 2 + mineheight
         mvprintw(maxy/2-fieldheight/2+field.mines[i]/fieldwidth, maxx/2-fieldwidth/2+field.mines[i]%fieldwidth, "+");
     }
 }
